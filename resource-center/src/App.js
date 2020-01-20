c159747a4ec8b9b1856a501a9e6458a42b290a46
benklinton <benklinton@gmail.com>
2020-01-20 16:40:39 -0700

    created header component

--- resource-center/src/App.js
+++ resource-center/src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Header from './components/Header';
+import images from './images.json';
 
-function App() {
+
+class App extends Component {
+  state = {
+    images
+  }
+  
+ render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Header images= {(this.state.images[0].img)}
+   />
   );
 }
-
+}
 export default App;
