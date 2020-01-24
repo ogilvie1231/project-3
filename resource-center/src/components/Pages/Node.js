78d4ec79e7925612cf96f3ec1bdd08b24deac0ad
chason <chasonlsmith@gmail.com>
2020-01-23 22:46:25 -0700

    converted MySQL, node, and sequalize pages. Working on components

---
+++ resource-center/src/components/Pages/Node.js
@@ -0,0 +1,113 @@
+import React from "react";
+
+function Node () {
+    return (
+      <div>
+        {/* <meta charSet="UTF -8" />
+        <title>FSWD-Library</title>
+        {/*LINKS*/}
+        {/*Bootstrap CSS*/}
+        {/* <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous" /> */}
+        {/*MY CSS*/}
+        {/* <link href="style.css" type="text/css" rel="stylesheet" /> */}
+        <ul>
+          <li style={{textAlign: 'center', font: 'bolder', background: '#039be5', color: 'white', height: '50px', fontSize: '24px', paddingTop: '3px'}}>
+            FullStackDev</li>
+          <li><a href="intro.html">Introduction</a></li>
+          <li><a href="vsc.html">Visual Studio Code</a></li>
+          <li><a href="gitbash.html">GitBash</a></li>
+          <li><a href="index.html">HTML</a></li>
+          <li><a href="css.html">CSS</a></li>
+          <li><a href="bootstrap.html">BOOTSTRAP</a></li>
+          <li><a href="javascript.html">JAVASCRIPT</a></li>
+          <li><a href="jQuery.html">jQuery</a></li>
+          <li><a href="firebase.html">Firebase</a></li>
+          <li><a href="Node.js.html">Node.js</a></li>
+          <li><a href="mysql.html">MySQL</a></li>
+          <li><a href="sequelize.html">Sequelize</a></li>
+        </ul>
+        <div style={{marginLeft: '18%', marginRight: '2%', padding: '1px 16px', height: '1000px'}}>
+          <div className="content">
+            <div className="card">
+              <div className="card-header">
+                <h1>Node.js</h1>
+                <img src="assets/node logo.jpg" className="titleimg" />
+              </div>
+              <div className="card-body">
+                <h5 className="card-title">Quick Links</h5>
+                <p className="card-text">You can find more info, documents and very detailed videos in the buttons
+                  below.</p>
+                <a href="https://nodejs.org/en/" className="btn btn-primary" target="_blank">Go to Node.js</a>
+                <a href="https://nodejs.org/en/docs/" className="btn btn-success" target="_blank">Node Documents</a>
+                <a href="https://www.youtube.com/playlist?list=PL55RiY5tL51oGJorjEgl6NVeDbx_fO5jR&ref=hackr.io" className="btn btn-info" target="_blank">Node Videos</a>
+                <a href="https://www.w3schools.com/nodejs/default.asp" className="btn btn-secondary" target="_blank">Node
+                  W3Schools</a>
+              </div>
+            </div>
+            <br />
+            <h3>Videos</h3>
+            <p>Notice that this div element has a left margin of 25%. This is because the side navigation is set to 25%
+              width. If you remove the margin, the sidenav will overlay/sit on top of this div.</p>
+            <p>Also notice that we have set overflow:auto to sidenav. This will add a scrollbar when the sidenav is too
+              long (for example if it has over 50 links inside of it).</p>
+            <div className="container">
+              <div className="card-deck">
+                <div className="card">
+                  <div className="card-body">
+                    <div className="embed-responsive embed-responsive-16by9">
+                      <iframe width={320} height={215} src="https://www.youtube.com/embed/R9GYRBDqxI8" frameBorder={0} allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+                    </div>
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">This card has supporting text below as a natural lead-in to additional
+                      content.</p>
+                    <hr />
+                    <a className="btn btn-success" href="https://www.youtube.com/watch?v=R9GYRBDqxI8&list=PLsrZV8shpwjNuvhvotOuzRCQGcqscCekJ" role="button" target="_blank">More
+                      videos Here</a>
+                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                  </div>
+                </div>
+                <div className="card">
+                  <div className="card-body">
+                    <div className="embed-responsive embed-responsive-16by9">
+                      <iframe width={320} height={215} src="https://www.youtube.com/embed/w-7RQ46RgxU" frameBorder={0} allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+                    </div>
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">This card has supporting text below as a natural lead-in to additional
+                      content.
+                    </p>
+                    <hr />
+                    <a className="btn btn-success" href="https://www.youtube.com/playlist?list=PL4cUxeGkcC9gcy9lrvMJ75z9maRw4byYp" role="button" target="_blank">More
+                      videos Here</a>
+                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                  </div>
+                </div>
+                <div className="card">
+                  <div className="card-body">
+                    <div className="embed-responsive embed-responsive-16by9">
+                      <iframe width={320} height={215} src="https://www.youtube.com/embed/65a5QQ3ZR2g" frameBorder={0} allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+                    </div>
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">This card has supporting text below as a natural lead-in to additional
+                      content.</p>
+                    <hr />
+                    <a className="btn btn-success" href="https://www.youtube.com/watch?v=65a5QQ3ZR2g&list=PL55RiY5tL51oGJorjEgl6NVeDbx_fO5jR" role="button" target="_blank">More
+                      videos Here</a>
+                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <br />
+            <p>Some text..</p>
+            <div className="code" style={{width: '50%'}}>
+              <code>
+                hello hello hello
+              </code>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+export default Node;
