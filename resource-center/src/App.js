911d74f89a2858d0594e5a7f647f90546b28b438
chason <chasonlsmith@gmail.com>
2020-01-25 12:15:50 -0700

    copied other pages from lucas branch into components/pages folder.

--- resource-center/src/App.js
+++ resource-center/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from './components/Header';
 import images from './images.json';
 import Sidebar from './components/Sidebar';
+import Card from './components/Cards';
 // import FireBase from './components/Pages/Firebase';
 // import Html from './components/Pages/IntroHTML';
 // import Intro from './components/Pages/CodingIntro';
@@ -14,9 +15,9 @@ class App extends Component {
  render() {
   return (
     <div>
-   <Header images= {(this.state.images[0].img)}
-   />
+   <Header images= {(this.state.images[0].img)} />
    <Sidebar />
+   {/* <Card /> */}
    {/* <FireBase /> */}
    {/* <Html /> */}
   </div>
