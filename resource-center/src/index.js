89db14948ef6b508a668ef5fd02ddd0d158c0189
ogilvie1231 <ogilvie1231@gmail.com>
2020-01-25 13:30:15 -0700

    working on auth0

--- resource-center/src/index.js
+++ resource-center/src/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Auth0Provider } from "./context/auth0-context";
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Auth0Provider>
+    <App />
+  </Auth0Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-
