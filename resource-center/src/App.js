3f93b071d7f83c97e9430bb083a3e8008930af80
ogilvie1231 <ogilvie1231@gmail.com>
2020-01-26 20:51:16 -0700

    working

--- resource-center/src/App.js
+++ resource-center/src/App.js
@@ -1,32 +1,25 @@
-
-import React, { Component, useContext } from "react";
-import { Auth0Context } from "./context/auth0-context";
+import React, { Component } from 'react';
 import Header from './components/Header';
 import images from './images.json';
 import Sidebar from './components/Sidebar';
-// import Card from './components/Cards';
-import FireBase from './Pages/Firebase';
-import Html from './Pages/IntroHTML';
-import Intro from './Pages/CodingIntro';
-import Btrap.js from './Pages/Btrap.js';
-
-function App() {
-  const auth0 = useContext(Auth0Context);
+import Intro from './Pages/intro';
 
 
+
+class App extends Component {
+  state = {
+    images
+  }
+  
+ render() {
   return (
     <div>
-    <div className="container text-center">{auth0.message}</div>
-    </div>
-   <Header images= {(this.state.images[0].img)} />
-   <Sidebar />
-   {/* <Card /> */}
-   <FireBase />
-   <Html />
+   <Header images= {(this.state.images[0].img)}
+   />
+   <Sidebar/>
    <Intro />
-   <Btrap.js />
   </div>
   );
 }
-
+}
 export default App;
