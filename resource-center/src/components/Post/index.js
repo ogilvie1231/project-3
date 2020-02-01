ff599a8a9f8640b4b2329f579cea0b4729f0e47a
benklinton <benklinton@gmail.com>
2020-02-01 10:40:54 -0700

    added summary input

--- resource-center/src/components/Post/index.js
+++ resource-center/src/components/Post/index.js
@@ -19,8 +19,11 @@ const Post = () => {
                    <h3> <label htmlFor="Link" id="title">Link</label> </h3>
                     <input type="text" className="form-control" id= "input"></input>
                 </div>
-                <div class="form-group form-check">
+                <div class="form-group">
+                   <h3> <label htmlFor="Summary" id="title">Summary</label> </h3>
+                    <input type="text" className="form-control" id= "summary"></input>
                 </div>
+                <div class="form-group form-check"></div>
                 <button type="submit" className="btn btn-primary" id="button">Submit</button>
             </form>
         </div>
