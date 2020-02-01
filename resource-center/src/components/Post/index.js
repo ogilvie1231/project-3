cd56354ad9dc4292e4377468122f01cb511c4c01
benklinton <benklinton@gmail.com>
2020-02-01 11:28:47 -0700

    created category field

--- resource-center/src/components/Post/index.js
+++ resource-center/src/components/Post/index.js
@@ -19,6 +19,21 @@ const Post = () => {
                    <h3> <label htmlFor="Link" id="title">Link</label> </h3>
                     <input type="text" className="form-control" id= "input"></input>
                 </div>
+                <h3> <label htmlFor="Link" id="title1">Category</label> </h3>
+                <select id="category">
+                <option value="Visual Studio Code">Visual Studio Code</option>
+                <option value="GitBash">GitBash</option>
+                <option value="HTML">HTML</option>
+                <option value="CSS">CSS</option>
+                <option value="Bootstrap">Bootstrap</option>
+                <option value="Javascript">Javascript</option>
+                <option value="jQuery">jQuery</option>
+                <option value="Firebase">Firebase</option>
+                <option value="Node.js">Node.js</option>
+                <option value="MySQL">MySQL</option>
+                <option value="Sequelize">Sequelize</option>
+
+                </select>
                 <div class="form-group">
                    <h3> <label htmlFor="Summary" id="title1">Summary</label> </h3>
                     <input type="text" className="form-control" id= "summary"></input>
