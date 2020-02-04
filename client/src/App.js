65b9a73a9be03a7d00388b336d5656c436cb4ead
Lucas Di Fini <difinile@gmail.com>
2020-02-03 21:37:16 -0700

    new update

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,8 @@ import Intro from "./Pages/intro";
 import images from "./images.json";
 import Sidebar from "./components/Sidebar";
 import NavBar from "./components/NavBar";
-<<<<<<< HEAD
 import { useAuth0 } from "./react-auth0-spa";
 import { Router, Route, Switch } from "react-router-dom";
-=======
-import Profile from "./components/Profile";
-import PrivateRoute from "./components/PrivateRoute";
-import images from "./images.json";
->>>>>>> staging
 import Home from "./Pages/Home";
 import Profile from "./components/Profile";
 import ajax from "./Pages/Ajax";
@@ -29,16 +23,9 @@ import Node from "./Pages/Node";
 import Sequelize from "./Pages/Sequelize";
 import Timers from "./Pages/Timers";
 import vsc from "./Pages/vsc";
-<<<<<<< HEAD
 import history from "./utils/history";
 import PrivateRoute from "./components/PrivateRoute";
 // import Intro from './Pages/IntroHTML';
-=======
-import Post from "./components/Post"
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Html from './Pages/IntroHTML';
-
->>>>>>> staging
 
 
 // class App extends Component {
@@ -81,24 +68,14 @@ function App() {
           <Route path="/CSSPge" component={CSSPge} />
           <Route path="/Firebase" component={Firebase} />
           <Route path="/GitBash" component={GitBash} />
-<<<<<<< HEAD
           <Route path="/IntroHTML" component={IntroHTML} />
           <Route path="/ReactPg" component={ReactPg} />
           <Route path="/jQPge" component={jQPge} />
           <Route path="/JSPg" component={JSPg} />
-=======
-          {/* <Route path="/IntroHTML" component={IntroHTML} /> */}
-          <Route path="/jquery" component={jQuery} />
-          <Route path="/javascript" component={JavaScript} />
->>>>>>> staging
           <Route path="/Mysql" component={Mysql} />
           <Route path="/Node" component={Node} />
           <Route path="/Sequelize" component={Sequelize} />
-<<<<<<< HEAD
           <Route path="/TimersPg" component={TimersPg} />
-=======
-          <Route path="/timer" component={Timers} />
->>>>>>> staging
           <Route path="/vsc" component={vsc} />
           <PrivateRoute path="/profile" component={Profile} />
           {/* <PrivateRoute path="/post" component={Post} /> */}
