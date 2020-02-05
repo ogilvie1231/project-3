16a692a40372440a87d5e8faf365465317347fa2
chason <chasonlsmith@gmail.com>
2020-02-04 18:59:29 -0700

    fixed home route button

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -21,12 +21,12 @@ const NavBar = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="/home">Home</Nav.Link>
               {/* <Nav.Link href="#link">Link</Nav.Link> */}
               <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                {/* <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item> */}
                 <NavDropdown.Item href="#action/3.2">
-                  <li><Link to="/home">Home</Link></li>
+                  {/* <li><Link to="/home">Home</Link></li> */}
                   {/* <li><Link to="/intro">Introduction</Link></li> */}
                   <li><Link to="/vsc">Visual Studio Code</Link></li>
                   <li><Link to="/gitbash">GitBash</Link></li>
@@ -71,16 +71,31 @@ const NavBar = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
+              <Nav.Link href="/home">Home</Nav.Link>
+              {/* <Nav.Link href="#link">Link</Nav.Link> */}
               <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                {/* <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item> */}
                 <NavDropdown.Item href="#action/3.2">
-                  Another action
+                  {/* <li><Link to="/home">Home</Link></li> */}
+                  {/* <li><Link to="/intro">Introduction</Link></li> */}
+                  <li><Link to="/vsc">Visual Studio Code</Link></li>
+                  <li><Link to="/gitbash">GitBash</Link></li>
+                  <li><Link to="/IntroHTML">HTML</Link></li>
+                  <li><Link to="/css">CSS</Link></li>
+                  <li><Link to="/Bootstrap">BOOTSTRAP</Link></li>
+                  <li><Link to="/javascript">JAVASCRIPT</Link></li>
+                  <li><Link to="/jQuery">jQuery</Link></li>
+                  <li><Link to="/react">React</Link></li>
+                  <li><Link to="/ajax">Ajax</Link></li>
+                  <li><Link to="/firebase">Firebase</Link></li>
+                  <li><Link to="/Node.js">Node.js</Link></li>
+                  <li><Link to="/mysql">MySQL</Link></li>
+                  <li><Link to="/sequelize">Sequelize</Link></li>
+                  <li><Link to="/profile">Profile</Link></li>
                 </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
+                {/* <NavDropdown.Item href="#action/3.3">
                   Something
-                </NavDropdown.Item>
+                </NavDropdown.Item> */}
                 <NavDropdown.Divider />
                 <NavDropdown.Item href="#action/3.4">
                   Separated link
