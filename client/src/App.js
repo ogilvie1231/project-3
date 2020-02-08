b554e113299e0c8ea5eb342469c32cd70b1ffebd
chason <chasonlsmith@gmail.com>
2020-02-08 10:23:42 -0700

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,8 @@ import { Router, Route, Switch } from "react-router-dom";
 import Home from "./Pages/Home";
 import Profile from "./components/Profile";
 import ajax from "./Pages/Ajax";
-import BootStrap from "./Pages/Bootstrap";
+import Boot from "./Pages/Bootstrap";
+// import CodingIntro from "./Pages/CodingIntro";
 import Css from "./Pages/CSS";
 import Firebase from "./Pages/Firebase";
 import GitBash from "./Pages/GitBash";
@@ -22,60 +23,65 @@ import vsc from "./Pages/vsc";
 import history from "./utils/history";
 import PrivateRoute from "./components/PrivateRoute";
 import JSpge from "./Pages/JavaScript";
-import Post from "./components/Post"
 // import Intro from './Pages/IntroHTML';
 
 
-class App extends Component {
-  state = {
-    title: "",
-    link: "",
-    catagory: "",
-    summary: "",
-  };
+// class App extends Component {
+//   state = {
+//     images
+//   };
 
+//   render() {
+//     return (
+//       <div>
+//         <Header images={this.state.images[0].img} />
+//         <Sidebar />
+//         {/* <Intro /> */}
+//       </div>
+//     );
+//   }
+// }
+// export default App;
 
-  Auth0 = () => {
-    const { loading } = useAuth0();
+function App() {
+  const { loading } = useAuth0();
 
-    if (loading) {
-      return <div>Loading...</div>;
-    }
+  if (loading) { 
+    return <div>Loading...</div>;
   }
-  render() {
-    return (
-      <div className="App">
-        <Router history={history}>
-          <header>
-            <NavBar />
-          </header>
-          {/* <Header images={images[0].img} /> */}
-          {/* <Sidebar /> */}
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/home" component={Home} />
-            {/* <Route path="/intro" component={Intro} /> */}
-            <Route path="/ajax" component={ajax} />
-            <Route path="/bootstrap" component={BootStrap} />
-            <Route path="/css" component={Css} />
-            <Route path="/firebase" component={Firebase} />
-            <Route path="/gitbash" component={GitBash} />
-            <Route path="/introHTML" component={IntroHTML} />
-            <Route path="/jquery" component={jQPge} />
-            {/* <Route path="/javascript" component={JSpge} /> */}
-            <Route path="/mysql" component={Mysql} />
-            <Route path="/node" component={Node} />
-            <Route path="/sequelize" component={Sequelize} />
-            <Route path="/timersPg" component={TimersPg} />
-            <Route path="/vsc" component={vsc} />
-            <Route path="/post" component={Post} />
-            <PrivateRoute path="/profile" component={Profile} />
-            {/* <PrivateRoute path="/post" component={Post} /> */}
-          </Switch>
-        </Router>
-      </div>
-    );
-  }
+
+  return (
+    <div className="App">
+      <Router history={history}>
+        <header>
+          <NavBar />
+        </header>
+        {/* <Header images={images[0].img} /> */}
+        {/* <Sidebar /> */}
+        <Switch>
+          <Route path="/home" component={Home} />
+          {/* <Route path="/intro" component={Intro} /> */}
+          <Route path="/ajax" component={ajax} />
+          <Route path="/BootStrap" component={Boot} />
+          {/* <Route path="/CodingIntro" component={CodingIntro} /> */}
+          <Route path="/Css" component={Css} />
+          <Route path="/JavaScript" component={JSpge} />
+          <Route path="/Firebase" component={Firebase} />
+          <Route path="/GitBash" component={GitBash} />
+          <Route path="/IntroHTML" component={IntroHTML} />
+          <Route path="/jQPge" component={jQPge} />
+          <Route path="/Mysql" component={Mysql} />
+          <Route path="/Node" component={Node} />
+          <Route path="/Sequelize" component={Sequelize} />
+          <Route path="/TimersPg" component={TimersPg} />
+          <Route path="/vsc" component={vsc} />
+          <PrivateRoute path="/profile" component={Profile} />
+          {/* <PrivateRoute path="/post" component={Post} /> */}
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
+
