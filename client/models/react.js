0e2a51a24caaadd75c741bdd9ee8d7c2e217f9e0
benklinton <benklinton@gmail.com>
2020-02-08 12:23:10 -0700

    fixed schemas

--- client/models/react.js
+++ client/models/react.js
@@ -5,7 +5,7 @@ const reactSchema = new Schema({
   title: { type: String, required: true },
   link: { type: String, required: true },
   category: { type: String, required: true },
-  summary: { type: Date, default: Date.now }
+  summary: { type: String, required: true }
 });
 
 const ReactModel = mongoose.model("ReactModel", reactSchema);
