382762014aefe6005c462fbad5ac615548df9e05
chason <chasonlsmith@gmail.com>
2020-02-10 17:23:02 -0700

    add postcss plugin for tailwind

--- client/src/components/Cards/index.js
+++ client/src/components/Cards/index.js
@@ -1,7 +1,6 @@
 import React from "react";
-import './style.css'
 
-function Card (props) {
+function Card () {
     return (
       <div class="max-w-sm w-full lg:max-w-full lg:flex">
       <div class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style="background-image: url('/img/card-left.jpg')" title="Woman holding a mug">
@@ -12,7 +11,6 @@ function Card (props) {
             <svg class="fill-current text-gray-500 w-3 h-3 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
               <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
             </svg>
-            Members only
           </p>
           <div class="text-gray-900 font-bold text-xl mb-2">test</div>
           <p class="text-gray-700 text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
