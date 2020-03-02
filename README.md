5a0e5bab543b73276b8ef7be0a9300b72a6cc849
ogilvie1231 <ogilvie1231@gmail.com>
2020-03-01 17:16:41 -0700

    updating readme

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # project-3
+
+<h2> <a href="https://lit-scrubland-80180.herokuapp.com/">A resource center for developers</a></h2>
+<p>The Resource Center was built as a place to gather helpful links and information for developers.
+<img src="./public/assets/images/14.png" alt="site image" >&nbsp;
+<!-- ![Image description](./public/assets/images/14.png) -->
+
+<h3>This project was built with:</h3>
+<ul>
+    <li>React</li>
+    <li>HTML</li>
+    <li>JavaScript</li>
+    <li>CSS</li>
+    <li>Reactstrap</li>
+    <li>mongoDB</li>
+    <li>Mongoose</li>
+    <li>Auth0</li>
+    <li>Express</li>
+    <li>Node</li>
+    <li>Hosted on Heroku</li>
+</ul>
+<h3>Group Members</h3>
+<ul>
+    <li>Alex Ogilvie</li>
+    <li>Ben Linton</li>
+    <li>Chason Law</li>
+    <li>Lucas Difini</li>
+</ul>
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -73,24 +73,19 @@ const NavBar = () => {
                     </li>
                   </NavDropdown.Item>
                   <NavDropdown.Item href="#action/3.3">
-                    {/* dropdown action link */}
                   </NavDropdown.Item>
                   <NavDropdown.Divider />
-                  {/* <NavDropdown.Item href="#action/3.4">
-                    Separated link
-                  </NavDropdown.Item> */}
                 </NavDropdown>
-              {/* </Button> */}
             </Nav>
             
-            <Form inline>
+            {/* <Form inline>
               <FormControl
                 type="text"
                 placeholder="Search"
                 className="mr-sm-2"
               />
               <Button variant="outline-success">Search</Button>
-            </Form>
+            </Form> */}
             
             <Button
               style={{ marginLeft: "5px" }}
@@ -160,17 +155,10 @@ const NavBar = () => {
                       <Link to="/profile=p">Profile</Link>
                     </li>
                   </NavDropdown.Item>
-                  {/* <NavDropdown.Item href="#action/3.3=p">
-                  Something
-                </NavDropdown.Item> */}
-                  {/* <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">
-                    Separated link
-                  </NavDropdown.Item> */}
                 </NavDropdown>
               </Button>
             </Nav>
-
+{/* 
             <Form inline>
               <FormControl
                 type="text"
@@ -178,7 +166,7 @@ const NavBar = () => {
                 className="mr-sm-2"
               />
               <Button variant="outline-success">Search</Button>
-            </Form>
+            </Form> */}
             <Button
               variant="outline-success"
               style={{ marginLeft: "5px" }}
@@ -198,73 +186,3 @@ const NavBar = () => {
 
 export default NavBar;
 
-// {!isAuthenticated && (
-//   <nav className="navbar navbar-expand-lg navbar-light bg-light">
-//     <a className="navbar-brand" href="#">
-//       Navbar
-//     </a>
-
-//     <button
-//       className="btn btn-primary btn-lg"
-//       // class="navbar-toggler"
-//       type="button"
-//       data-toggle="collapse"
-//       data-target="#navbarNav"
-//       aria-controls="navbarNav"
-//       aria-expanded="false"
-//       aria-label="Toggle navigation"
-//       onClick={() => loginWithRedirect({})}
-//     >
-//       Log in
-//     </button>
-
-//     <span>
-//       <button
-//         className="btn btn-primary btn-lg"
-//         // class="navbar-toggler"
-//         type="button"
-//         data-toggle="collapse"
-//         data-target="#navbarNav"
-//         aria-controls="navbarNav"
-//         aria-expanded="false"
-//         aria-label="Toggle navigation"
-//       >
-//         <Link className="homeBtn" to="/home">
-//           Home
-//         </Link>
-//       </button>
-//     </span>
-
-//     {/* <div className="collapse navbar-collapse" id="navbarNav">
-//       <ul className="navbar-nav">
-//         <li className="nav-item active">
-//           <a className="nav-link" href="#">
-//             Home <span className="sr-only">(current)</span>
-//           </a>
-//         </li>
-//         <li className="nav-item">
-//           <a className="nav-link" href="#">
-//             Features
-//           </a>
-//         </li>
-//         <li className="nav-item">
-//           <a className="nav-link" href="#">
-//             Pricing
-//           </a>
-//         </li>
-//         <li className="nav-item">
-//           <a className="nav-link disabled" href="#">
-//             Disabled
-//           </a>
-//         </li>
-//       </ul>
-//     </div> */}
-//   </nav>
-// )}
-// {isAuthenticated && (
-//   <span>
-//     <Link to="/">Home</Link>&nbsp;
-//     <Link to="/profile">Profile</Link>
-//     <button onClick={() => logout()}>Log out</button>
-//   </span>
-// )}
